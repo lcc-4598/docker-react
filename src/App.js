1026e022db3b33e6db0961452b1aa9d34f6aa045
Lucca <lucca.chabu.santos@gmail.com>
2020-09-30 18:24:10 -0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          kkk foi memo oi
+          Feature branch worked!
         </p>
         <a
           className="App-link"
